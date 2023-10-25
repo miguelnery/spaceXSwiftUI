75b19718b0bcda0fae84290e4be295fe764aeaa1
miguelnery <nery.mik@gmail.com>
2023-10-25 17:45:35 -0300

    add generated assets

--- SpaceXLaunches/UICommons/Icon.swift
+++ SpaceXLaunches/UICommons/Icon.swift
@@ -3,7 +3,7 @@ import UIKit
 extension UIImage {
     enum Icon {
         static let success = UIImage(imageLiteralResourceName: "success")
-//        UIImage(named: "success")
         static let failure = UIImage(imageLiteralResourceName: "failure")
+        static let unknown = UIImage(imageLiteralResourceName: "unknown")
     }
 }
