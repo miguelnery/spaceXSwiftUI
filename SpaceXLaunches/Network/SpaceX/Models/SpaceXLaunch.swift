29b39c445aad6c638c35fc5bd94f651d0e7fcd61
miguelnery <nery.mik@gmail.com>
2023-10-25 18:02:01 -0300

    add convenience type MissionSuccess to SpaceXLaunch

--- SpaceXLaunches/Network/SpaceX/Models/SpaceXLaunch.swift
+++ SpaceXLaunches/Network/SpaceX/Models/SpaceXLaunch.swift
@@ -2,7 +2,7 @@ struct SpaceXLaunch: Codable {
     let name: String
     let dateUtc: String
     let rocket: String
-    let success: Bool?
+    let success: MissionSuccess
     let links: Links
 }
 
@@ -13,6 +13,33 @@ extension SpaceXLaunch {
         struct Patch: Codable {
             let small: String?
             let large: String?
+        }
+    }
+}
+
+extension SpaceXLaunch {
+    enum MissionSuccess: Codable {
+        case success
+        case failure
+        case unknown
+        
+        var icon: ImageAsset {
+            switch self {
+            case .success:
+                return Asset.success
+            case .failure:
+                return Asset.failure
+            case .unknown:
+                return Asset.unknown
+            }
+        }
+        
+        init(_ value: Bool?) {
+            guard let value = value else {
+                self = .unknown
+                return
+            }
+            self = value ? .success : .failure
         }
     }
 }
