7ff7c15010edec774976fa358f804ffa789e0a1e
miguelnery <nery.mik@gmail.com>
2023-11-12 22:09:39 -0300

    fix icons alignment on LaunchView

--- SpaceXLaunches/LaunchList/LaunchView.swift
+++ SpaceXLaunches/LaunchList/LaunchView.swift
@@ -8,17 +8,20 @@ struct LaunchView: View {
             AsyncImage(url: model.iconURL) { image in
                 image
                     .resizable()
-                    .frame(width: 35, height: 35)//, alignment: .bottom)
+                    .frame(width: 35, height: 35)
             } placeholder: {
                 Color(.gray)
-                    .frame(width: 35, height: 35)//, alignment: .center)
+                    .frame(width: 35, height: 35)
                     .clipShape(RoundedRectangle(cornerRadius: 0.75))
             }
             InfoHStack(model: model.infoFields)
             Spacer()
             Image(uiImage: model.missionStatus)
                 .resizable()
-                .frame(width: 35, height: 35, alignment: .top)
+                .frame(width: 35, height: 35)
+                .alignmentGuide(VerticalAlignment.center) { context in
+                    context[.bottom] * 1.2
+                }
         }
     }
 }
